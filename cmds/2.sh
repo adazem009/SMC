68f9c15e29a1942f5fbe5e03496ab89e1c111c30
adazem009 <68537469+adazem009@users.noreply.github.com>
2020-10-31 15:52:44 +0100

    Bug fixes

--- cmds/2.sh
+++ cmds/2.sh
@@ -8,3 +8,15 @@ process_argument ${command[1]}
 loops_i[${#loops_i[@]}]="1"
 loops_id[${#loops_id[@]}]="$i1"
 loops_max[${#loops_max[@]}]="${argument[0]}"
+if ((${argument[0]} <= 0)); then
+	pos=0
+	until [ "${command[0]}" = "3" ] && [ "$pos" = "0" ]; do
+		i1="$(($i1+1))"
+		process_command "${PRG[$(($i1-1))]}"
+		if [[ "${command[0]}" = "2" ]] || [[ "${command[0]}" = "7" ]]; then
+			pos="$(($pos+1))"
+		elif [[ "${command[0]}" = "3" ]] && ((pos > 0)); then
+			pos="$(($pos-1))"
+		fi
+	done
+fi
