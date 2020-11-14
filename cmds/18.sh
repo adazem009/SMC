7acdaf2f15b5a638abfea32e719565dee1ebf6df
adazem009 <68537469+adazem009@users.noreply.github.com>
2020-11-14 21:44:24 +0100

    Switch string and letter

--- cmds/18.sh
+++ cmds/18.sh
@@ -6,7 +6,7 @@
 # ---
 source ./f.sh
 process_argument "${command[1]}"
-temp4="${argument[0]:$((${argument[1]}-1)):1}"
+temp4="${argument[1]:$((${argument[0]}-1)):1}"
 process_argument "${command[2]}"
 i3=0
 while ((i3 < ${#argument[@]})); do
