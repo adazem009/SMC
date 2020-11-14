f8937ecd6861d8b506def5d3dec6e2fa138dcf18
adazem009 <68537469+adazem009@users.noreply.github.com>
2020-11-14 18:57:59 +0100

    Add get letter command

---
+++ cmds/18.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# ---
+# Get letter of a string and save it in variable(s).
+# 12/[string],[letter]/[variable(s)]
+# ---
+source ./f.sh
+process_argument "${command[1]}"
+temp4="${argument[0]:$((${argument[1]}-1)):1}"
+process_argument "${command[2]}"
+i3=0
+while ((i3 < ${#argument[@]})); do
+	i3=$((i3+1))
+	set_var "${argument[$((i3-1))]}" "$temp4"
+done
