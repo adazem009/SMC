4567a6e6a9f2980bdfc6e2098168aa9272b015d9
adazem009 <68537469+adazem009@users.noreply.github.com>
2020-11-15 12:21:41 +0100

    Remove debug output

--- cmds/22.sh
+++ cmds/22.sh
@@ -10,11 +10,9 @@ temp4="${argument[0]}"
 process_argument "${command[1]}"
 echo > ./misc/.list && rm ./misc/.list
 i3=0
-echo "$(wc -l < "./misc/.lists/$temp4")"
 tmplen="$(wc -l < "./misc/.lists/$temp4")"
 while ((i3 < tmplen)); do
 	i3=$((i3+1))
-	echo $i3
 	if [[ "$i3" = "${argument[0]}" ]]; then
 		echo "${argument[1]}" >> "./misc/.list"
 	else
