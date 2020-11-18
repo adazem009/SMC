7078813c724bd6fb2c3c362877680e87a6ea21ca
adazem009 <68537469+adazem009@users.noreply.github.com>
2020-11-18 08:42:41 +0100

    Add missing quotes ( fix #11 )

--- cmds/16.sh
+++ cmds/16.sh
@@ -8,11 +8,11 @@ i3=1
 cmdlentemp="${#command[@]}"
 while ((i3 < cmdlentemp)); do
 	i3="$(($i3+1))"
-	process_argument ${command[$(($i3-1))]}
+	process_argument "${command[$(($i3-1))]}"
     setas=""
     for (( countme=1; countme<=${#argument[@]}-1; countme++ ))
     do  
-       setas+=${argument[${countme}]}
+       setas+="${argument[${countme}]}"
     done
-	set_var ${argument[0]} ${setas}
+	set_var "${argument[0]}" "${setas}"
 done
